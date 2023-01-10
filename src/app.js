71ab373d55592e1bf6b37a217a2e02f3dc6bea0a
tarikochuery <ttarikechuery@gmail.com>
2023-01-10 00:31:13 +0000

    Configuração inicial do servidor

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
